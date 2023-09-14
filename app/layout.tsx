c3379139cbf3ce3735b05c9c70eb12e16e8bc21b
Josh M <joshua0833@hotmail.com>
2023-09-14 16:20:26 -0700

    removed unused font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter, Roboto } from 'next/font/google'
+import {Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
 
 const roboto = Roboto({ 
   weight: ['100', '300', '400', '500', '700', '900'], 
